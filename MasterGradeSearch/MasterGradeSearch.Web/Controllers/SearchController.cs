a62eefa96ce99fbb050d397c1d5df2348c95a914
Kirill Maloyaroslavtsev <k.maloyaroslavtsev@pflb.ru>
2015-05-29 20:52:13 +0300

    Работа над Search

---
+++ MasterGradeSearch/MasterGradeSearch.Web/Controllers/SearchController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using MasterGradeSearch.Web.Models;
+
+namespace MasterGradeSearch.Web.Controllers
+{
+    public class SearchController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Search
+        public async Task<ActionResult> Index()
+        {
+            var courses= await db.Courses.Include(c=>c.Institute).Include(c=>c.Discipline).Include(c=>c.Exams).ToListAsync();
+            var model = new SearchViewModel();
+            model.FindedCourses = courses;
+            return View(model);
+        }
+    }
+}
---
+++ MasterGradeSearch/MasterGradeSearch.Web/Models/SearchViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using MasterGradeSearch.Core.Commons;
+
+namespace MasterGradeSearch.Web.Models
+{
+    public class SearchViewModel
+    {
+        public List<Course> FindedCourses { get; set; }
+    }
+
+    public class SearchCourseViewModel :Course
+    {
+        [NotMapped]
+        public double SearchCoef { get; set; }
+    }
+}
