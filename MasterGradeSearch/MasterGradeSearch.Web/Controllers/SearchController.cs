52a1a894e8d68753e38f6dcd0471f6e53e5958aa
Kirill Maloyaroslavtsev <k.maloyaroslavtsev@pflb.ru>
2015-06-01 21:27:40 +0300

    Какие-то небольшие наработки

--- MasterGradeSearch/MasterGradeSearch.Web/Controllers/SearchController.cs
+++ MasterGradeSearch/MasterGradeSearch.Web/Controllers/SearchController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
@@ -17,15 +18,29 @@ namespace MasterGradeSearch.Web.Controllers
         // GET: Search
         public async Task<ActionResult> Index()
         {
-            var model = new SearchViewModel {Cities = await db.Cities.ToListAsync(), FindedCourses = new List<Course>()};
+            var cities = await db.Cities.ToListAsync();
+            var model = new SearchViewModel { CitiesSelectList = new List<SelectListItem>(), FindedCourses = new List<Course>() };
+            foreach (var city in cities)
+            {
+                var selectItem = new SelectListItem();
+                selectItem.Text = city.Name;
+                selectItem.Value = city.Id.ToString();
+                model.CitiesSelectList.Add(selectItem);
+            }
+            
             return View(model);
         }
 
         [AcceptVerbs(HttpVerbs.Get)]
-        public JsonResult LoadPhysiansByDepartment(string deptId)
+        public JsonResult LoadDistinctsByCity(string cityId)
         {
             //Your Code For Getting Physicans Goes Here
-            var districts = db.Districts.ToList();
+            if (string.IsNullOrEmpty(cityId))
+            {
+                return Json(new List<SelectListItem>(), JsonRequestBehavior.AllowGet);
+            }
+            var intCityId = Int32.Parse(cityId);
+            var districts = db.Districts.Where(d=>d.CityId==intCityId).ToList();
 
 
             var phyData = districts.Select(m => new SelectListItem()
--- MasterGradeSearch/MasterGradeSearch.Web/Models/SearchViewModel.cs
+++ MasterGradeSearch/MasterGradeSearch.Web/Models/SearchViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Web.Mvc;
 using MasterGradeSearch.Core.Commons;
 
 namespace MasterGradeSearch.Web.Models
@@ -9,6 +10,7 @@ namespace MasterGradeSearch.Web.Models
         public List<City> Cities { get; set; }
         public List<District> Districts { get; set; } 
         public List<Course> FindedCourses { get; set; }
+        public List<SelectListItem> CitiesSelectList { get; set; }
     }
 
     public class SearchCourseViewModel :Course
